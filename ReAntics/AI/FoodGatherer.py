1c4e88530a33f0d1e34e5fd0f06ebf5a06cb50e6
mikaylawhiteaker <mikayla.whiteaker@gmail.com>
2018-10-28 16:10:36 -0600

    started work

--- ReAntics/AI/FoodGatherer.py
+++ ReAntics/AI/FoodGatherer.py
@@ -49,7 +49,7 @@ class AIPlayer(Player):
             return [(0,0), (5, 1), 
                     (0,3), (1,2), (2,1), (3,0),
                     (0,2), (1,1), (2,0),
-                    (0,1), (1,0) ]
+                    (0,1), (1,0)]
         elif currentState.phase == SETUP_PHASE_2:
             numToPlace = 2
             moves = []
